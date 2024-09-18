be34256c35dedcacc046f19ec004fafa136fcd7d
Hasan Batal <hh0657768@gmail.com>
2024-09-18 00:46:22 -0700

    adding constant string

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,23 @@
+const kOnloadingTitleMessage = "Define yourself in your unique way";
+const kTermAndPolicyMessage =
+    "By signing up you agree to our Terms,Privacy Policy and Cookie Use";
+const kForgetPasswordMessage =
+    "Enter your email for the verification process, We will send 4 digits code to your email";
+const kResetPasswordMessage =
+    "Set the new password for your account so you can login and access all the features.";
+const kPasswordChangedMessage =
+    "Your can now use your new password to login to your account.";
+const kEmptyNotificationMessage = "You haven’t gotten any notifications yet!";
+const kAlertNotificationMessage =
+    "We’ll alert you when something cool happens.";
+const kEmptyDataErrorMessage = "There is no Data right Now";
+const kEmptySavedProductsMessage =
+    "You don’t have any saved items. Go to home and add some.";
+const kEmptyCartMessage = "Your Cart Is Empty!";
+
+const kAddAddressSucessMessage =
+    'Congratulations! Your new address has been added.';
+
+const kAddCardSucessMessage = 'Congratulations! Your new Card has been added.';
+const kAddOrderSucessMessage =
+    'Congratulations! Your new Order has been added.';

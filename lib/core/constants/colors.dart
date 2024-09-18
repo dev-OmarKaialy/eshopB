6e902c942c238d0c459b8f4b79cc9d318900b980
Hasan Batal <hh0657768@gmail.com>
2024-09-18 00:46:02 -0700

    adding color platte class

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorPlatte {
+  static const Color kBlackColor = Color(0xFF1A1A1A);
+  static const Color kRedColor = Color(0xFFED1010);
+  static const Color kGreenColor = Color(0xFF0C9409);
+  static const Color kBlueColor = Color(0xFF1877F2);
+  static const Color kPrimaryColor = Color.fromARGB(255, 124, 59, 182);
+}

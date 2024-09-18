a91071338a73a0611bd30c1bebbf023501ea9b23
Hasan Batal <hh0657768@gmail.com>
2024-09-18 00:47:33 -0700

    adding padding file

---
+++ lib/core/constants/padding.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const double kPaddingHorizontal = 12;
+const double kPaddingTop = 40.0;
+
+EdgeInsets kPadding = const EdgeInsets.only(
+    top: 40, left: kPaddingHorizontal, right: kPaddingHorizontal);

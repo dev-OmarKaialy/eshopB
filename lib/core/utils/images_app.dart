953d9708bf05136f1613e67104ed51ccafdfea24
Hasan Batal <hh0657768@gmail.com>
2024-09-18 00:49:00 -0700

    add image class manger and style text manger

---
+++ lib/core/utils/images_app.dart
@@ -0,0 +1,9 @@
+abstract class ImageApp {
+  static const String logo1 = "assets/images/logo.png";
+  static const String logo2 = "assets/images/logo2.png";
+  static const String logo3 = "assets/images/logo3.png";
+  static const String noWifi = "assets/images/no_wifi.png";
+  static const String addreess = "assets/images/address.png";
+  static const String facebookLogo = "assets/images/facebook.png";
+  static const String googleLogo = "assets/images/google.png";
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle textStyle36 = const TextStyle(
+      fontSize: 36, fontFamily: "Marah", fontWeight: FontWeight.bold);
+  static TextStyle textStyle24 = const TextStyle(
+    fontSize: 24,
+    fontFamily: "Marah",
+    color: Colors.black,
+  );
+
+  static TextStyle textStyle20 = const TextStyle(
+      fontSize: 20, fontFamily: "Marah", fontWeight: FontWeight.bold);
+  static TextStyle textStyle18 = const TextStyle(
+      fontSize: 18, fontFamily: "Marah", fontWeight: FontWeight.w600);
+  static TextStyle textStyle16 = const TextStyle(
+    fontSize: 16,
+    fontFamily: "Marah",
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle textStyle14 = const TextStyle(
+    fontSize: 14,
+    fontFamily: "Marah",
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+  static TextStyle textStyle12 = const TextStyle(
+    fontSize: 12,
+    fontFamily: "Marah",
+    fontWeight: FontWeight.normal,
+  );
+}

8b415b39c1dfb2006f9d27f558ae7a032b3da2bb
jwankhalil <jwan8khalil@gmail.com>
2024-09-19 16:58:30 +0300

    core folder

---
+++ lib/core/config/app_assets.dart
@@ -0,0 +1,6 @@
+abstract class AppAssets {
+  static const String imagesFolder = 'assets/images';
+
+  static const String appLogo = '$imagesFolder/app-logo.png';
+  static const String onboarding = '$imagesFolder/onboarding.png';
+}
---
+++ lib/core/utils/app_localizaition.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppLocalizaition {
+  static const arLocal = Locale("ar");
+  static const enLocale = Locale("en");
+  static List<Locale> get supportedLocales => [arLocal, enLocale];
+}
